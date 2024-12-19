8632e2698e61f9416f2307f4d64a9e6bc22df9f5
Dhammabhushan9 <dhammwagh922004@gmail.com>
2024-12-19 09:03:53 +0530

    update in deployement

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,10 +3,10 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   const token = req.header('Authorization');
   if (!token) return res.status(401).json({ error: 'Access denied' });
-  const key=process.env.JWT_SECRET;
+  const key="DHAMMABHUSHAN";
   console.log(key)
   try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    const verified = jwt.verify(token,"DHAMMABHUSHAN");
     req.user = verified;
     next();
   } catch (err) {
